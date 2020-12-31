90072c40910039c55e651d5248dc9a5079a1ec31
Xingsheng Zhao <xingshengzhao@gmail.com>
2020-12-31 10:30:38 -0600

    test bench edit

--- testbench/matrix_all.sh
+++ testbench/matrix_all.sh
@@ -1,6 +1,9 @@
-sudo ./mem_bench --type=fillseqNT --mode=matrix --thread=16  | tee fillseqNT_matrix.log
-sudo ./mem_bench --type=fillseqWB --mode=matrix --thread=16  | tee fillseqWB_matrix.log
-sudo ./mem_bench --type=fillrandomNT --mode=matrix --thread=16  | tee fillrandomNT_matrix.log
-sudo ./mem_bench --type=fillrandomWB --mode=matrix --thread=16  | tee fillrandomWB_matrix.log
-sudo ./mem_bench --type=readseqNT --mode=matrix --thread=16 --load=true --initfile=true | tee readseqNT_matrix.log
-sudo ./mem_bench --type=readrandomNT --mode=matrix --thread=16 --load=true --initfile=true | tee readrandomNT_matrix.log
+# use 'numactl' to run this script on a single socket
+
+THREAD_NUM=8
+sudo ./mem_bench --type=fillseqNT --mode=matrix --thread=$THREAD_NUM  | tee fillseqNT_matrix.log
+sudo ./mem_bench --type=fillseqWB --mode=matrix --thread=$THREAD_NUM  | tee fillseqWB_matrix.log
+sudo ./mem_bench --type=fillrandomNT --mode=matrix --thread=$THREAD_NUM  | tee fillrandomNT_matrix.log
+sudo ./mem_bench --type=fillrandomWB --mode=matrix --thread=$THREAD_NUM  | tee fillrandomWB_matrix.log
+sudo ./mem_bench --type=readseqNT --mode=matrix --thread=$THREAD_NUM --load=true --initfile=true | tee readseqNT_matrix.log
+sudo ./mem_bench --type=readrandomNT --mode=matrix --thread=$THREAD_NUM --load=true --initfile=true | tee readrandomNT_matrix.log
