f676d7d203abcb68a71b11420364b7826c59ff3e
Xingsheng Zhao <xingshengzhao@gmail.com>
2021-04-06 11:03:10 -0400

    add latency

--- testbench/read_latency.sh
+++ testbench/read_latency.sh
@@ -4,11 +4,11 @@ SOCKET_NO=0
 # Insert 100 milliion and, each thread read 10 million
 
 
-numactl -N $SOCKET_NO sudo ../Dash/release/ycsb_bench  --thread=16 --benchmarks=load,readrandom,readlat,readnonlat --stats_interval=10000000 --read=10000000 --num=100000000 | tee read_latency.dash
+# numactl -N $SOCKET_NO sudo ../Dash/release/ycsb_bench  --thread=16 --benchmarks=load,readrandom,readlat,readnonlat --stats_interval=10000000 --read=10000000 --num=100000000 | tee read_latency.dash
 
-# numactl -N $SOCKET_NO sudo ../release/hash_bench  --thread=16 --benchmarks=load,readrandom,readlat,readnonlat --stats_interval=10000000 --read=10000000 --num=100000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee read_latency.turbo
+numactl -N $SOCKET_NO sudo ../release/hash_bench  --thread=16 --benchmarks=load,readrandom,readlat,readnonlat --stats_interval=10000000 --read=10000000 --num=100000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee read_latency.turbo
 
-# numactl -N $SOCKET_NO sudo ../release/hash_bench_30  --thread=16 --benchmarks=load,readrandom,readlat,readnonlat --stats_interval=10000000 --read=10000000 --num=100000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee read_latency.turbo30
+numactl -N $SOCKET_NO sudo ../release/hash_bench_30  --thread=16 --benchmarks=load,readrandom,readlat,readnonlat --stats_interval=10000000 --read=10000000 --num=100000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee read_latency.turbo30
 
 # numactl -N $SOCKET_NO sudo ../CCEH-PMDK/ycsb_bench --thread=16 --benchmarks=load,readrandom,readlat,readnonlat --stats_interval=10000000 --read=10000000 --num=100000000 | tee read_latency.cceh
 
--- testbench/ycsb.sh
+++ testbench/ycsb.sh
@@ -5,11 +5,11 @@ SOCKET_NO=0
 
 t=20
 
-# numactl -N $SOCKET_NO sudo ../release/hash_bench  --thread=$t --benchmarks=load,ycsbd,ycsba,ycsbb,ycsbc,ycsbf --stats_interval=10000000 --read=10000000 --num=120000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee ycsb.turbo
+numactl -N $SOCKET_NO sudo ../release/hash_bench  --thread=$t --benchmarks=load,ycsbd,ycsba,ycsbb,ycsbc,ycsbf --stats_interval=10000000 --read=10000000 --num=120000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee ycsb.turbo
 
-# numactl -N $SOCKET_NO sudo ../release/hash_bench_30  --thread=$t --benchmarks=load,ycsbd,ycsba,ycsbb,ycsbc,ycsbf  --stats_interval=10000000 --read=10000000 --num=120000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee ycsb.turbo30
+numactl -N $SOCKET_NO sudo ../release/hash_bench_30  --thread=$t --benchmarks=load,ycsbd,ycsba,ycsbb,ycsbc,ycsbf  --stats_interval=10000000 --read=10000000 --num=120000000 --bucket_count=65536 --cell_count=16 --no_rehash=false | tee ycsb.turbo30
 
-numactl -N $SOCKET_NO sudo ../Dash/release/ycsb_bench  --thread=$t --benchmarks=load,ycsbd,ycsba,ycsbb,ycsbc,ycsbf --stats_interval=10000000 --read=10000000 --num=120000000 | tee ycsb.dash
+# numactl -N $SOCKET_NO sudo ../Dash/release/ycsb_bench  --thread=$t --benchmarks=load,ycsbd,ycsba,ycsbb,ycsbc,ycsbf --stats_interval=10000000 --read=10000000 --num=120000000 | tee ycsb.dash
 
 # numactl -N $SOCKET_NO sudo ../CCEH-PMDK/ycsb_bench --thread=$t --benchmarks=load,ycsbd,ycsba,ycsbb,ycsbc,ycsbf --stats_interval=10000000 --read=10000000 --num=120000000 | tee ycsb.cceh
 
