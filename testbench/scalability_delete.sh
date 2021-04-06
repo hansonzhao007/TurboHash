78e17dd3863c9483fcaa90d6afa9c8319a7053e8
Xingsheng Zhao <xingshengzhao@gmail.com>
2021-04-05 22:48:56 -0400

    update turbo data

--- testbench/scalability_delete.sh
+++ testbench/scalability_delete.sh
@@ -3,7 +3,7 @@ SOCKET_NO=0
 
 # Insert 120 milliion and, each thread read 10 million
 
-for t in 1 2 4 8 16 20 24 28 32 36 40
+for t in 40
 do
     # numactl -N $SOCKET_NO sudo ../Dash/release/ycsb_bench  --thread=$t --benchmarks=load,delete  --stats_interval=10000000 --read=10000000 --num=120000000 | tee thread_delete.dash_$t
 
